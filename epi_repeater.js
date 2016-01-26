35c1913ec45c222bdf7368c594b6e21f2c353958
Milo Verbowski <milov@outlook.com>
2016-01-26 17:04:25 -0500

    yui

--- epi_repeater.js
+++ epi_repeater.js
@@ -77,6 +77,7 @@ function HackGetRepeater(repeaterLabel) {
 }
 
 function ModifyValue(modValue) {
+    alert('modValue hit: ' + modValue);
     $axure.internal(function($ax) {
         var temp = function() {
             $ax(function(obj) {
