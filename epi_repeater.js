7c4f30ac9bc1b59acdd51d94ca4569aa22791007
Milo Verbowski <milov@outlook.com>
2016-01-26 02:16:21 -0500

    uy

--- epi_repeater.js
+++ epi_repeater.js
@@ -4,6 +4,48 @@ function isEpi_Repeater() {
     alert('epi_repeater!');
 };
 
+function FakeCellData() {
+    this.column0 = {
+        text,
+        type,}
+};
+
+
+var fakeCellData = FakeCellData()[2];
+fakeCellData.text[0] = "butthole";
+fakeCellData.text[1] = "butthole2";
+fakeCellData.type[0] = "peni";
+fakeCellData.type[1] = "peni2";
+alert(fakeCellData.text[0]);
+alert(fakeCellData.text[1]);
+alert(fakeCellData.type[0]);
+alert(fakeCellData.type[1]);
+
+
+function FakeSource() {
+    this.adaptiveStyles = {},
+    this.data = [2],
+    this.dataProps = ["column0"],
+    this.id = "ed147f6bd3704d85a4705e176a56d077",
+    this.label = "rep2",
+    this.objects = [],
+    this.owner = [],
+    this.parent = [],
+    this.repeaterPropMap = [],
+    this.scriptIds = ["u27"],
+    this.style = {
+        location: { x: "250", y: "210"},
+        size: {height: "150", width: "250"},
+    },
+    this.styleType = "repeater",
+    this.type = "repeater",
+    this.visible = "true"
+};
+
+
+
+var row = new Object();
+
 var masterRepeater = {
 	label: masterRepeaterLabel,
 	obj: getRepeater(masterRepeaterLabel),
