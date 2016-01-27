3981bf2cbd2aedf7586ed2848bbbce1a68b24967
Milo Verbowski <milov@outlook.com>
2016-01-27 18:55:17 -0500

    fds

--- epi_repeater.js
+++ epi_repeater.js
@@ -37,16 +37,13 @@ function AddRowToDataSet(targetRepeater, colValue, value) {
 
 function AddJsonToRepeater(targetRepeater, jsonData)
 {
-
-    var len = jsonData.carListing.car.length;
-    var items = [];
-
     for (var i = 0; i < len; i++) {
+        var count = 0;
+        for (var item in jsonData.car[i]) {
+            epi.repeater.AddRowToDataSet(targetRepeater, [column][count], jsonData.car[i][item]);
 
-        var value = jsonData.carListing.car[i].manufacturer;
-        ModifyActiveDataSet(targetRepeater, value, number);
-        number += 1;
-
+            count += 1;
+        }
     }
 }
 
